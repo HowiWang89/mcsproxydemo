9a3adc3ac9280270278af7c5fdfb77870410fb33
HowiWang89 <howiwang@163.com>
2021-08-30 19:16:25 +0800

    08303

--- src/main/java/com/huifu/mcsproxydemo/domain/Main.java
+++ src/main/java/com/huifu/mcsproxydemo/domain/Main.java
@@ -5,9 +5,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World friend");
         System.out.println("难搞哦");
+        System.out.println("搞好了");
     }
 
-    public static int hao(){
+    public static int hao() {
         return 1;
     }
 
